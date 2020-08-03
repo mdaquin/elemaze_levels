f18c61d31a522b5f43da1ea57ac3f827df8bb5bf
mdaquin <mathieu@daquin.net>
2020-08-03 22:30:13 +0100

    new results and small (untested) fix

---
+++ 4cd-config.py
@@ -0,0 +1,42 @@
+
+# weights for fitness
+# distance to the ideal number of steps of the solution
+wsolution = 0.70
+#    - compactness of the level
+wsize = 0.20
+#    - use to the guys (ration of the guys present that are moving)
+wuse = 0.1
+
+# chances that a guy is used in first random generation
+p_g_guy = 1.0
+
+# chances that a cell will contain something originally
+p_g_cell = 1.0
+
+# chances each direction has to be enabled originally
+p_g_dir = 0.5
+
+# generation size
+g_size = 30
+
+# generation number
+g_num = 100
+
+# ideal number of steps for each solution
+idealsol = 12
+
+# mutation rate
+mut_rate = 0.3
+
+# crossover rate
+co_rate = 0.2
+
+# dimentions of the grid
+maxx = 5
+maxy = 5
+
+# show solving state after
+sssa = 1000
+
+# stop solving after
+ssa = 4000
