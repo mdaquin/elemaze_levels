e1b6ae5cb4ae455222342ba07c991574ec2d9411
Mathieu d'Aquin <mathieu@daquin.net>
2020-09-06 08:36:36 +0100

    updating results

--- get_level_with.py
+++ get_level_with.py
@@ -11,6 +11,7 @@ for d in os.listdir(sys.argv[1]):
         with open (sys.argv[1]+"/"+d+"/"+f) as file:
             data = json.load(file)
             if "nb_steps" in data and data["nb_steps"] == int(sys.argv[2]):
+                print(d+"/"+f)
                 sol = solver.astar_solve(data)
                 seq = solver.getSequence(sol)
                 print(d+"/"+f+" - "+seq)
